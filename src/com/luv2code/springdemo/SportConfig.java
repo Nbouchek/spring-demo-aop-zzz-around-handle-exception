6cdca85410455e67e1d3c831f435299ba8ef1a98
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-01 11:33:09 +0100

    Add beans to the config class.

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,10 +1,22 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
+//@ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
+    // define bean for our sad fortune service
+    @Bean
+    public FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
+
+    // define bean for swimCoach AND inject dependency
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
 }
