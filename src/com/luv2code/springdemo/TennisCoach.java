ea789128e6d0171cafeb8f6c2b3391ebf5589faf
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-01 10:16:27 +0100

    Demo @PostConstruct and  @PreDestroy - @Autowired and @Qualifier-.

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,16 +2,28 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
     @Autowired
     @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
+    // define init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: inside doMyStartupStuff()");
+    }
+
+    // define destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside doMyCleanupStuff()");
+    }
     // define a default constructor
     public TennisCoach() {
         System.out.println("TennisCoach: inside default constructor");
