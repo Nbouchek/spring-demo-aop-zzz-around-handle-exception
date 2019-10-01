3188f1a27af43bb526a1d6ed9f44fdf5d1b86d2f
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-01 09:33:58 +0100

    Inject random fortune service using @Autowired and @Qualifier to resolve multiple implementations ussue.

---
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+    // create an array of strings
+    private String[] data = {
+      "Beware of the wolf in sheep's clothing",
+      "Diligence is the mother of good ends",
+      "The journey is the reward"
+    };
+
+    // create a random number generator
+    private Random myRandom = new Random();
+    @Override
+    public String getFortune() {
+        int index = myRandom.nextInt(data.length);
+        String theFortune = data[index];
+        return theFortune;
+    }
+}
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
     @Autowired
-    @Qualifier("happyFortuneService")
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
     // define a default constructor
@@ -34,6 +34,6 @@ public class TennisCoach implements Coach {
 
     @Override
     public String getDailyFortune() {
-        return "Today is a great day";
+        return fortuneService.getFortune();
     }
 }
