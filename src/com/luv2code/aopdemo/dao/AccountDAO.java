7b1e6ae9f73de9f02f566845f9f9c9bfdf0c8907
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-11 11:29:26 +0100

    demo on @AfterReturning

--- src/com/luv2code/aopdemo/dao/AccountDAO.java
+++ src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -3,11 +3,31 @@ package com.luv2code.aopdemo.dao;
 import com.luv2code.aopdemo.Account;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class AccountDAO {
     private String name;
     private String serviceCode;
 
+
+    // add a new method: findAccounts
+    public List<Account> findAccounts() {
+        List<Account> myAccounts = new ArrayList<>();
+
+        // create sample accounts
+        Account temp1 = new Account("Linda", "Gold");
+        Account temp2 = new Account("Adam", "Gold");
+        Account temp3 = new Account("Hana", "Gold");
+
+        // add them to our accounts list
+        myAccounts.add(temp1);
+        myAccounts.add(temp2);
+        myAccounts.add(temp3);
+
+        return myAccounts;
+    }
 
     public void addAccount(Account theAccount, boolean vipFlag) {
         System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
