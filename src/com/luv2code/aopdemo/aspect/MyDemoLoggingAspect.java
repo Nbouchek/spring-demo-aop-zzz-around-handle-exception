70de0f69d68a5ba84aff368a27adfd8b8fb2d8a2
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-11 06:53:57 +0100

    Pointcut declaration demo.

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,14 +2,18 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
     // this is where we add all of our related advices for logging
-    // let's start with a @Before advice aspect
-    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    // Create a pointcut declaration
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====> Executing @Before advice on addAccount()");
     }
